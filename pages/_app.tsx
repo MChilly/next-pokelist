d860b46b604ba4c158f9804cf73c75430460a08e
MChilly <mariatsili89@gmail.com>
2024-09-06 22:53:35 +0200

    Final changes-added a favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Head>
+      {/* Favicon */}
+      <link rel="icon" href="/pokeball_favicon.ico" />
+      {/* Optionally, other favicons (e.g., for Apple devices) */}
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+    </Head>
+    <Component {...pageProps} />
+  </>
+);
 }
+
+  
